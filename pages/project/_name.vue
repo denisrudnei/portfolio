33199b8637481f383a05e16b796937d3e1ba0604
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2019-10-27 10:01:25 -0300

    Added fullscreen image

--- pages/project/_name.vue
+++ pages/project/_name.vue
@@ -16,8 +16,37 @@
           {{ project.name }}
         </v-card-title>
         <v-card-text v-html="project.description" />
+        <v-card-actions v-if="actual !== ''">
+          <v-btn class="primary white--text" tile block @click="dialog = true">
+            Exibir imagem em tela cheia
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
+    <v-dialog :v-model="dialog" :value="dialog" fullscreen scrollable>
+      <v-card tile>
+        <v-toolbar>
+          <v-toolbar-items>
+            <v-btn icon @click="dialog = false">
+              <v-icon>close</v-icon>
+            </v-btn>
+            <v-btn icon :disabled="!hasPrev()" @click="prev">
+              <v-icon>keyboard_arrow_left</v-icon>
+            </v-btn>
+            <v-btn icon :disabled="!hasNext()" @click="next">
+              <v-icon>keyboard_arrow_right</v-icon>
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-img v-if="actual !== ''" :src="getImage(actual)" />
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>  
+    </v-dialog>
   </v-row>
 </template>
 
@@ -26,7 +55,8 @@ export default {
   auth: false,
   data() {
     return {
-      actual: ''
+      actual: '',
+      dialog: false
     }
   },
   asyncData({ $axios, params }) {
@@ -43,6 +73,32 @@ export default {
     },
     setActual(name) {
       this.actual = name
+    },
+    hasPrev() {
+      const index = this.project.images.findIndex(img => {
+        return this.actual === img
+      })
+      if (this.project.images[index - 1]) return true
+      return false
+    },
+    hasNext() {
+      const index = this.project.images.findIndex(img => {
+        return this.actual === img
+      })
+      if (this.project.images[index + 1]) return true
+      return false
+    },
+    prev() {
+      const index = this.project.images.findIndex(img => {
+        return img === this.actual
+      })
+      this.actual = this.project.images[index - 1]
+    },
+    next() {
+      const index = this.project.images.findIndex(img => {
+        return img === this.actual
+      })
+      this.actual = this.project.images[index + 1]
     }
   }
 }
